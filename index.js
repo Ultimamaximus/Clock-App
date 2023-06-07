2268ebc45647d9913e0a436b88ee35e247dde643
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2023-06-07 13:08:56 -0700

    I added comments and refactored the code.

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
+// Caching DOM elements
+const clockElement = document.getElementById("Clock");
+const dateElement = document.getElementById("Date");
+
+// Function to display current time
 function currentTime() {
-    let date = new Date(); /* creating object of Date class */
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    document.getElementById("Clock").innerText = hour + " : " + min + " : " + sec; /* links time to the div */
-    let t = setTimeout(function(){ currentTime() }, 1000); /* updates the clock every second */
-    let midday = "AM"; /* sets Am as default */
-    midday = (hour >= 12) ? "PM" : "AM"; /* if condition is met midday becomes PM otherwise it stays AM */
-    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* Makes 0 = 12 and prevents * > 12 from showing up */
+  // Getting current time
+  let date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+
+  // Determining AM or PM
+  let midday = (hour >= 12) ? "PM" : "AM";
+
+  // Formatting time with leading zeros
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+
+  // Converting hour to 12-hour format
+  hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+
+  // Creating the time string and updating the clock element
+  const currentTimeString = hour + " : " + min + " : " + sec + " " + midday;
+  clockElement.innerText = currentTimeString;
+
+  // Getting current date
+  let currentDate = new Date();
+  let cDay = currentDate.getDate();
+  let cMonth = currentDate.getMonth() + 1;
+  let cYear = currentDate.getFullYear();
+
+  // Updating the date element
+  dateElement.innerText = cMonth + "/" + cDay + "/" + cYear;
+
+  // Scheduling the function to run again after 1 second
+  setTimeout(currentTime, 1000);
+}
+
+// Function to add leading zeros to numbers less than 10
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
   }
-  
-  function updateTime(k) {
-    if (k < 10) {
-      return "0" + k;
-    }
-    else {
-      return k;
-    }
-  }
-  
-  currentTime(); /* calling currentTime() function to initiate the process */
+}
 
-
-let currentDate = new Date();
-let cDay = currentDate.getDate();
-let cMonth = currentDate.getMonth() + 1;
-let cYear = currentDate.getFullYear();
-document.getElementById("Date").innerText = cMonth + "/" + cDay + "/" + cYear;
-
+// Calling currentTime() function to initiate the process
+currentTime();
